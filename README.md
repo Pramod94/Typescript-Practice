02af15734184ca29bed9690938aef24f74ef0009
Pramod94 <pramodmithyantha@gmail.com>
2022-02-07 17:44:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-qxachr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qxachr)
---
+++ index.ts
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+// Union types
+type InputUnion = number | string;
+
+let input: InputUnion = 10;
+input = 'Hello';
+
+// throws error because acceptable is not of type boolean
+//input = true;
+
+// type literals
+
+type Literals = 20 | 40 | 60;
+
+let input2: Literals = 20;
+input2 = 40;
+
+// throws error because can't accept of value 50
+//input2 = 50;
+
+
